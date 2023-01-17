0c5f7e1d4689cee85fe2be6d3c187d5a158a55c1
EddieTheCubeHead <eetu.asikainen1204@gmail.com>
2023-01-17 18:34:57 +0200

    Create bot

---
+++ README.md
@@ -0,0 +1,5 @@
+# MustacheBot
+
+Quick and dirty discord bot for helping with organizing the Koodausta ja Kisailua 2023 event.
+
+I am writing this readme first and I can already guarantee here the code won't be pretty.
---
+++ bot.py
@@ -0,0 +1,50 @@
+import json
+
+import discord
+from discord import Client, Intents, Interaction, TextChannel
+from discord.app_commands import CommandTree, describe
+
+with open("settings.json", "r", encoding="utf-8") as settings_file:
+    settings = json.loads(settings_file.read())
+
+with open("secrets.json", "r", encoding="utf-8") as settings_file:
+    token = json.loads(settings_file.read())["token"]
+
+
+_GUILD = discord.Object(id=settings["guild"])
+
+
+class Bot(Client):
+    def __init__(self, description: str, intents: Intents):
+        super().__init__(description=description, intents=intents)
+        self.tree = CommandTree(self)
+
+    async def setup_hook(self):
+        self.tree.copy_global_to(guild=_GUILD)
+        await self.tree.sync(guild=_GUILD)
+
+
+def create_bot():
+    description = "Use /help or /summon to get help into your room"
+    intents = discord.Intents.default()
+    return Bot(description, intents)
+
+
+bot = create_bot()
+
+
+@bot.event
+async def on_ready():
+    print("Connected")
+
+
+@bot.tree.command(description="Ask for help", name="help")
+@describe(room="The number or name of the room your team is in")
+async def get_help(interaction: Interaction, room: str):
+    admin_channel = interaction.guild.get_channel(settings["admin_channel"])
+    await admin_channel.send(f"Help requested in room {room}")
+    await interaction.response.send_message("Help request sent to the organizers.", ephemeral=True)
+
+
+if __name__ == '__main__':
+    bot.run(token)
